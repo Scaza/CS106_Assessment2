329c4e14734fcc8ee4bc1e54ddf2f57b900b94cd
Scott Tutone <william.tutone@gmail.com>
2023-11-09 19:10:37 +1300

    First commit

---
+++ CS106_Assessment2/CS106_Assessment2.cpp
@@ -0,0 +1,11 @@
+// CS106_Assessment2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
